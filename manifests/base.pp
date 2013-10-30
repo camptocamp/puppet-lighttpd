753bb87f61e1f0192ac4efa69d695830e8e03d5e
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-10-30 09:45:35 +0100

    lint

--- manifests/base.pp
+++ manifests/base.pp
@@ -10,39 +10,39 @@ it's inherited by OS-dependent classes.
 */
 class lighttpd::base {
 
-  package {"lighttpd":
+  package {'lighttpd':
     ensure => present,
   }
 
-  service {"lighttpd":
+  service {'lighttpd':
     ensure => running,
     enable => true,
-    require => Package["lighttpd"],
+    require => Package['lighttpd'],
   }
-  exec {"reload-lighttpd":
-    command     => "/etc/init.d/lighttpd force-reload",
+  exec {'reload-lighttpd':
+    command     => '/etc/init.d/lighttpd force-reload',
     refreshonly => true,
-    onlyif      => "lighttpd-angel -t -f /etc/lighttpd/lighttpd.conf",
+    onlyif      => 'lighttpd-angel -t -f /etc/lighttpd/lighttpd.conf',
   }
 
-  file { ["/etc/lighttpd/vhosts", "/etc/lighttpd/vhosts-config"]:
+  file { ['/etc/lighttpd/vhosts', '/etc/lighttpd/vhosts-config']:
     ensure  => directory,
     owner   => root,
     group   => root,
-    mode    => 0755,
-    require => Package["lighttpd"],
+    mode    => '0755',
+    require => Package['lighttpd'],
   }
 
-  concat {"/etc/lighttpd/conf-available/00-puppet-vhost.conf":
+  concat {'/etc/lighttpd/conf-available/00-puppet-vhost.conf':
     owner   => root,
     group   => root,
-    mode    => 0644,
-    require => Package["lighttpd"],
+    mode    => '0644',
+    require => Package['lighttpd'],
   }
 
-  lighttpd::module{"puppet-vhost":
+  lighttpd::module{'puppet-vhost':
     ensure => present,
-    require => File["/etc/lighttpd/conf-available/00-puppet-vhost.conf"],
+    require => File['/etc/lighttpd/conf-available/00-puppet-vhost.conf'],
   }
 
 }
